82d0f03181806884d078eef8db30b7d8df5a3043
Nejc <nejcrogelsek0@gmail.com>
2021-01-18 22:14:55 +0100

    complete: loading screen

--- src/pages/LoadingScreen.jsx
+++ src/pages/LoadingScreen.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-const LoadingScreen = () => {
-  return <div>Loading screen</div>;
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > * + *": {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
 
-export default LoadingScreen;
+export default function CircularIndeterminate() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} id='loading-container'>
+      <CircularProgress />
+    </div>
+  );
+}
