e1526b63a1861aecc2bdc612357b4c760296ebc0
Nejc <nejcrogelsek0@gmail.com>
2021-01-18 15:12:40 +0100

    adding wiggli text and hero animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import image2 from "../assets/images/img2.jpg";
 import { Link as LinkS } from "react-scroll";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import Typical from "react-typical";
 
 const HeroSection = () => {
   const openResume = () => {
@@ -26,12 +27,26 @@ const HeroSection = () => {
           <div className='row'>
             <div className='col-12 hero-content'>
               <h1>
-                Nejc
+                <span class='wiggly-font'>N</span>
+                <span class='wiggly-font'>E</span>
+                <span class='wiggly-font'>J</span>
+                <span class='wiggly-font'>C</span>
                 <br />
-                Rogelšek
+                <span class='wiggly-font'>R</span>
+                <span class='wiggly-font'>O</span>
+                <span class='wiggly-font'>G</span>
+                <span class='wiggly-font'>E</span>
+                <span class='wiggly-font'>L</span>
+                <span class='wiggly-font'>Š</span>
+                <span class='wiggly-font'>E</span>
+                <span class='wiggly-font'>K</span>
               </h1>
               <div className='heroAnim-wrapper'>
-                <span>Full-Stack Developer</span>
+                <Typical
+                  loop={Infinity}
+                  steps={["Full Stack Developer", 1000, "Coder", 1000]}
+                  wrapper='p'
+                />
               </div>
               <button className='heroBtn-resume' onClick={openResume}>
                 Resume
