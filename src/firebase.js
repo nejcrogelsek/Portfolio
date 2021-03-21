f6f95f7716357d939579c178edd8ae418365e252
Nejc Rogelšek <nejcrogelsek0@gmail.com>
2021-03-22 00:36:16 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCkV5_Z7W4QYjOUX9MIW_ttF6HJEICXjCM",
-  authDomain: "portfolio-b037d.firebaseapp.com",
-  projectId: "portfolio-b037d",
-  storageBucket: "portfolio-b037d.appspot.com",
-  messagingSenderId: "858363463802",
-  appId: "1:858363463802:web:8a016958117fe4df24d3c3",
-  measurementId: "G-8G4VQ34MNQ",
+  
 });
 // Database
 const db = firebase.firestore();
